9b29adbbc14f95d0315b2f42d9ad09706f2aac0f
Jonas Collberg <jonas.collberg@gmail.com>
2014-09-03 21:21:41 +0200

    Calendar is now working

--- client/app/scripts/controllers/contacts.js
+++ client/app/scripts/controllers/contacts.js
@@ -1,12 +1,5 @@
 "use strict";
 
-/**
- * @ngdoc function
- * @name clientApp.controller:ContactsCtrl
- * @description
- * # ContactsCtrl
- * Controller for contact list
- */
 angular.module("clientApp")
     .controller("ContactsCtrl", ["$scope", "Contacts", function ($scope, Contacts) {
         $scope.contacts = Contacts.query();
