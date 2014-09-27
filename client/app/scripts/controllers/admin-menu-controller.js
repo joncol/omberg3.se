9f3367eb4f8508b9e3b291aeb4cef4d1194f7c55
Jonas Collberg <jonas.collberg@gmail.com>
2014-09-28 00:56:43 +0200

    Changed font sizes from px to em and disabled login/logout depending on status

--- client/app/scripts/controllers/admin-menu-controller.js
+++ client/app/scripts/controllers/admin-menu-controller.js
@@ -4,6 +4,13 @@ angular.module('clientApp')
     .controller('AdminMenuCtrl', ['$scope', '$http', '$location', '$window', 'UserService', 'AuthenticationService', function ($scope, $http, $location, $window, UserService, AuthenticationService) {
         $('[data-tooltip="tooltip"]').tooltip('hide');
         $('[data-toggle="tooltip"]').tooltip('hide');
+        // if ($window.sessionStorage.token) {
+        //     console.log('disabling login link');
+        //     $('[href="#/login"').removeAttr('href');
+        // } else {
+        //     console.log('disabling logout link');
+        //     $('[href="#/logout"').removeAttr('href');
+        // }
 
         $scope.isLoggedIn = function () {
             return AuthenticationService.isLoggedIn;
